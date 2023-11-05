03d656eb60bd3acb27803f348d29404fe7be5a04
Hedonical <phillip.post@gmail.com>
2023-11-05 01:24:31 -0400

    got table requests working

--- server/app.py
+++ server/app.py
@@ -1,58 +1,67 @@
 from shiny import App, render, ui, reactive
-import forex_python as fr
 import flag
 from country_dict import countries
+from exchange_rate import scrape_currency_conversion
 
 all_countries = countries()
 
-default_choices = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (k,v) in all_countries.all.items() if "" in k}
+default_choices = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (
+    k, v) in all_countries.all.items() if "" in k}
 
 
-app_ui = ui.page_fluid(
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.div({"style": "font-weight: bold; margin-left: 2px;"},
+               ui.input_numeric("am", "Amount", 1, min=0)),
+        ui.div({"style": "font-weight: bold; margin-left: 2px;"},
+               ui.input_text("z", "Filter", placeholder="Country Name"),
+               ui.input_select("x", "Your Currency", default_choices, selected="United States")),
+        ui.div({"style": "font-weight: bold; margin-left: 2px;"},
+               ui.input_text("m", "Filter", placeholder="Country Name"),
+               ui.input_select("y", "Convert to", default_choices, selected="Vietnam"))),
+    ui.card(
         ui.div({"style": "text-align: center;"},
                ui.markdown("# Current¢")),
-        ui.div( {"style": "font-weight: bold; margin-left: 2px;"},
-        ui.input_text("z", "Filter", placeholder="Country Name"),
-        ui.input_select("x", "Your Currency", default_choices)),
-        ui.div( {"style": "font-weight: bold; margin-left: 2px;"},
-        ui.input_text("m", "Filter", placeholder="Country Name"),
-        ui.input_select("y", "Convert to", default_choices)),
         ui.output_text_verbatim("txt")
+    )
+
 )
 
 
 def server(input, output, session):
     @output
     @render.text
-    def txt():
-        return f'x: "{input.x()}"'
-    
+    async def txt():
+        # calculate the currency conversion
+        output = await scrape_currency_conversion(all_countries.all[input.x()].curr,
+                                                  all_countries.all[input.y(
+                                                  )].curr,
+                                                  input.am())
+        return f'x: "{output}"'
+
     # update the Your currency input options based on text
-    @reactive.Effect
+    @reactive.Calc
     def _():
         filter_str = input.z()
-        filtered_input = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (k,v) in all_countries.all.items() if filter_str.lower() in k.lower()}
+        filtered_input = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (
+            k, v) in all_countries.all.items() if filter_str.lower() in k.lower()}
 
         ui.update_select(
             "x",
             choices=filtered_input,
         )
-        
-    @reactive.Effect
+
+    @reactive.Calc
     def _():
-        
+
         filter_str = input.m()
-        filtered_input = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (k,v) in all_countries.all.items() if filter_str.lower() in k.lower()}
+        filtered_input = {k: f"{flag.flag(v.code)} {v.name} {v.curr}" for (
+            k, v) in all_countries.all.items() if filter_str.lower() in k.lower()}
 
         ui.update_select(
             "y",
             choices=filtered_input,
         )
-
-
-
-
-
 
 
 app = App(app_ui, server, debug=True)
--- server/exchange_rate.py
+++ server/exchange_rate.py
@@ -1,68 +1,38 @@
-import requests
+import pyodide.http
 from bs4 import BeautifulSoup as BS
 from datetime import datetime, timedelta
 import pandas
 
 
-def scrape_currency_conversion(curr_from, curr_to, date, amount):
+async def scrape_currency_conversion(curr_from, curr_to, amount):
     # curr_from: three letter string representing the currency to convert from
     # curr_to: three letter string representing the currency to convert to
     # date: string in YYYY-MM-DD format
     # amount: string representing the starting amount of currency
 
     # make the url to scrape from the input
-    scrape_url = f"https://fx-rate.net/calculator/?c_input={curr_from}&cp_input={curr_to}&amount_from={amount}&amount_to=1000.00&date_input={date}&interbank_input=0"
+    scrape_url = f"https://currencyconvert.online/{curr_from}/{curr_to}/table"
 
     # keep on requesting until the response code is 200 (i.e. a valid response)
     response = 0
 
     while response != 200:
         # request the info from the page
-        page = requests.get(scrape_url)
-        response = page.status_code
+        page = await pyodide.http.pyfetch(scrape_url)
+        response = page.status
 
     # parse the page with beautifulsoup
-    soup = BS(page.text, "html.parser")
+    soup = BS(await page.string(), "html.parser")
 
     # find the resulting conversion value
-    value = float(soup.find('input', {'name': 'amount_to'}).get('value'))
+    value = soup.find("table", {"id": "table1-history"})
 
-    return value
+    # read the data into a data table
+    table = pandas.read_html(io=str(value))[0]
 
+    table["Price"] = table["Price"].str.extract(
+        r'(\d+\.*\d+)').astype(float).apply(lambda x: x*amount)
+    table["Date"] = table["Date"].str.extract(r'(\d+/\d+/\d+)')
+    table['Date'] = pandas.to_datetime(table['Date'], dayfirst=True)
 
-def scrape_currency_table(curr_from, curr_to, date, amount, span="week"):
-    data = {"year": [], "month": [], "day": [], "amount": []}
-
-    # convert the date to a datetime object
-    date = datetime.strptime(date, '%Y-%m-%d')
-
-    # create a dictionary to later convert to a pandas
-    if span == "week":
-        for i in range(7):
-            adjusted_date = date - timedelta(days=i)
-            # save the date to the data
-            data["year"].append(adjusted_date.year)
-            data["month"].append(adjusted_date.month)
-            data["day"].append(adjusted_date.day)
-            data["amount"].append(scrape_currency_conversion(curr_from,
-                                                             curr_to,
-                                                             adjusted_date.strftime(
-                                                                 "%Y-%m-%d"),
-                                                             amount))
-
-    elif span == "month":
-        for i in range(30):
-            adjusted_date = date - timedelta(days=i)
-            # save the date to the data
-            data["year"].append(adjusted_date.year)
-            data["month"].append(adjusted_date.month)
-            data["day"].append(adjusted_date.day)
-            data["amount"].append(scrape_currency_conversion(curr_from,
-                                                             curr_to,
-                                                             adjusted_date.strftime(
-                                                                 "%Y-%m-%d"),
-                                                             amount))
-
-    # turn the dictionary into a dataframe
-    table = pandas.DataFrame.from_dict(data)
     return table
